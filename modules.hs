c9a7673993dc329bc9190a85c24b7bde1fc83597
Jack Slingerland <jack.slingerland@gmail.com>
2021-07-19 07:04:32 -0400

    Modules

--- modules.hs
+++ modules.hs
@@ -1,4 +1,11 @@
 import Data.List
+import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs) = if key == k
+                            then Just v
+                            else findKey key xs
